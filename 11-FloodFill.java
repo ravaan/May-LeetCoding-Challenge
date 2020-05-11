f7e65f0ce400f5dee1834d88fe14c0b1d29bc06d
ravaan <arpit.agarwal181@gmail.com>
2020-05-11 21:31:19 +0530

    minor change

--- 11-FloodFill.java
+++ 11-FloodFill.java
@@ -1,30 +1,21 @@
 class Solution {
     boolean[][] visited;
-    int[][] ans;
     int r, c;
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         r = image.length;
         c = image[0].length;
         visited = new boolean[r][c];
-        ans = new int[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++)
-                ans[i][j] = image[i][j];
-        }
         helper(sr, sc, newColor, image[sr][sc], image);
-        return ans;
+        return image;
     }
 
     public void helper(int x, int y, int val, int sVal, int[][] image) {
         if (!isValid(x, y)) return;
         if (visited[x][y]) return;
         visited[x][y] = true;
-        if (sVal != image[x][y]) {
-            ans[x][y] = image[x][y];
-            return;
-        }
-        ans[x][y] = val;
+        if (sVal != image[x][y]) return;
+        image[x][y] = val;
         helper(x + 1, y, val, sVal, image);
         helper(x, y + 1, val, sVal, image);
         helper(x - 1, y, val, sVal, image);
