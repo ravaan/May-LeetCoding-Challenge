d727d1ca2eb8ebe5cd167cdf3e5b5c8522397b22
ravaan <arpit.agarwal181@gmail.com>
2020-05-12 17:31:25 +0530

    refactor for loop

--- 12-SingleElementInSortedArray.java
+++ 12-SingleElementInSortedArray.java
@@ -1,13 +1,8 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int ans = nums[0];
-        boolean flag = false;
-        // XOR all nums
-        for (int i : nums) {
-            if (flag)
-                ans ^= i;
-            flag = true;
-        }
+        for(int i = 1; i < nums.length; i++)
+            ans ^= nums[i];
         return ans;
     }
 }
