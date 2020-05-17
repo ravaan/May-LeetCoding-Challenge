4dd66da066aa5f3273c9ba8dac521342fc5d2b0a
ravaan <arpit.agarwal181@gmail.com>
2020-05-17 16:39:51 +0530

    add day 17

---
+++ 17-FindAllAnagramInAString.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int ls = s.length();
+        int lp = p.length();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if (ls < lp) return ans;
+
+        int[] pCount = new int[26];
+        int[] count = new int[26];
+
+        for (char ch : p.toCharArray()) pCount[ch - 'a']++;
+        int i = 0;
+        while (i < ls) {
+            if (i >= lp) {
+                if (Arrays.equals(pCount, count))
+                    ans.add(i - lp);
+                count[s.charAt(i - lp) - 'a']--;
+            }
+            count[s.charAt(i) - 'a']++;
+            i++;
+        }
+        if (Arrays.equals(pCount, count))
+            ans.add(i - lp);
+        return ans;
+    }
+
+    public boolean isSame(int[] x, int[] y) {
+        for (int i = 0; i < 26; i++)
+            if (x[i] != y[i])
+                return false;
+        return true;
+    }
+}
