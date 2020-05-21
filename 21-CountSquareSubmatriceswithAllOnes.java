4a2d80338da712d3ea2266eaeb5ef3342f15d037
ravaan <arpit.agarwal181@gmail.com>
2020-05-21 23:36:35 +0530

    add day 21

---
+++ 21-CountSquareSubmatriceswithAllOnes.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int countSquares(int[][] m) {
+        int row = m.length;
+        int col = m[0].length;
+        int ans = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (m[i][j] == 1 && i > 0 && j > 0) {
+                    m[i][j] = Math.min(m[i - 1][j - 1], Math.min(m[i - 1][j], m[i][j - 1])) + 1;
+
+                }
+                ans += m[i][j];
+            }
+        }
+        for (int[] a : m) System.out.println(Arrays.toString(a));
+        return ans;
+    }
+
+
+}
