fb31ae68554a0620839ae5f70178add88fa3e926
ravaan <arpit.agarwal181@gmail.com>
2020-05-28 16:11:16 +0530

    improve complexity

--- 28-CountingBits.java
+++ 28-CountingBits.java
@@ -2,22 +2,23 @@ class Solution {
     public int[] countBits(int num) {
         int[] ans = new int[num + 1];
         if (num == 0) return ans;
-
-        Map<Integer, Integer> bitMap = new HashMap<Integer, Integer>();
-
-        for (int i = num; i >= 0; i--) {
-            int n = i;
-            int count = 0;
-            while (n > 0) {
-                if (bitMap.containsKey(n)) {
-                    count += bitMap.get(n);
-                    break;
-                }
-                if ((n & 1) == 1) count++;
-                n = n >> 1;
-            }
-            ans[i] = count;
+        ans[1] = 1;
+        if (num == 1) return ans;
+        for (int i = 2; i <= num; i++) {
+            int lastN = ans[i >> 1];
+            if (i % 2 == 0) ans[i] = lastN;
+            else ans[i] = lastN + 1;
         }
 
         return ans;
     }
+}
+
+
+// 1 2 3 4 5 6 7
+// 1 1 2 1 2 2 3
+
+// 110
+// 11
+// 111
+// 11
